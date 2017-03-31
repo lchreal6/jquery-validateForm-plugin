b8f434efd14e88579c5e85a8ded279a4fd2bc708
lchreal6 <lchreal6@gmail.com>
2017-03-31 17:16:51 +0800

    change userId rex

--- plugin.js
+++ plugin.js
@@ -41,7 +41,7 @@
             this.rex = new RegExp("^1[0-9]{10}$");
             break;
             case "userID":
-            this.rex = new RegExp("^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$");
+            this.rex = new RegExp("^[0-9]{18}$");
             break;
             case "email":
             this.rex = new RegExp("^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$");
